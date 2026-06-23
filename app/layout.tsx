9118c7d70fc9c4fc2a197c1665e0a4e7813494a3
Jimmy Essel <jimmykodwoessel@gmail.com>
2026-06-23 09:14:18 +0000

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,10 +24,34 @@ const varelaRound = Varela_Round({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://sewdigital.com"),
   title: "SewDigital",
   description: "Next-generation tailor management",
   icons: {
     icon: "/stitchlogo.png",
+    apple: "/stitchlogo.png",
+  },
+  openGraph: {
+    title: "SewDigital",
+    description: "Next-generation tailor management",
+    url: "https://sewdigital.com",
+    siteName: "SewDigital",
+    images: [
+      {
+        url: "/tour.png", // Hero image
+        width: 1200,
+        height: 630,
+        alt: "SewDigital Hero Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SewDigital",
+    description: "Next-generation tailor management",
+    images: ["/tour.png"], // Hero image
   },
 };
 
